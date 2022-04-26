ac3c29581aa6d0457c42bc23496c9a66cf00faea
Sitanshu Tripathi <31413456+sitanshu-cse10@users.noreply.github.com>
2022-04-26 22:19:27 +0530

    Update streamlit_app.py
    Update iteration code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
-streamlit.title('My Parent New Health Dinner')
-                
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
