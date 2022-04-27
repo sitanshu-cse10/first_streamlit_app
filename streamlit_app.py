9ce08285c6ccb6c13c2bfab111cae691e8d9a74d
Sitanshu Tripathi <31413456+sitanshu-cse10@users.noreply.github.com>
2022-04-28 01:39:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-#streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -22,11 +22,15 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.text('🥑🍞 Fruit Adice')
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
 #streamlit.dataframe(my_fruit_list)
 
