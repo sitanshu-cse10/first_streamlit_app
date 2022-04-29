467457d71513ac20aa38da5c04e4938e15931bd0
Sitanshu Tripathi <31413456+sitanshu-cse10@users.noreply.github.com>
2022-04-29 14:35:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,25 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice !')
 
-fruit_choice=streamlit.text_input('What fruit would you like information about ?','kiwi')
+streamlit.header('Fruityvice Fruit Advice !')
 
-streamlit.write('The user entered',fruit_choice)
+try:
+  fruit_choice=streamlit.text_input('What fruit would you like information about ?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+fruit_choice)
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  streamlit.erro()
+
 
 streamlit.stop()
 
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+fruit_choice)
 
-#streamlit.text(fruityvice_response.json())
-
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(fruityvice_normalized)
+streamlit.write('The user entered',fruit_choice)
 #streamlit.dataframe(my_fruit_list)
 my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
 my_cur = my_cnx.cursor()
