388fb7c5131b1cf9746fed95fd7f49349e55c67b
Sitanshu Tripathi <31413456+sitanshu-cse10@users.noreply.github.com>
2022-04-29 17:55:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,17 @@ streamlit.header('Fruityvice Fruit Advice !')
 
 streamlit.header('Fruityvice Fruit Advice !')
 
+
+
+
+
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+fruit_choice)
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized   
+    
+
+
 try:
   fruit_choice=streamlit.text_input('What fruit would you like information about ?')
   if not fruit_choice:
